0989efe32c8d5e959035117b52d958150defd166
Anna Vasylenko <Anna_Vasylenko@epam.com>
2019-05-14 13:39:07 +0300

    removed hardcoded path to chromedriver

--- src/test/java/CheckGoogleSearch.java
+++ src/test/java/CheckGoogleSearch.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.CodeLocations;
@@ -22,15 +23,15 @@ import java.util.Map;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class CheckGoogleSearch extends JUnitStories {
-    private static WebDriver driver = driver();
+  private static WebDriver driver = driver();
 
   @Override
   public Configuration configuration() {
     return new MostUsefulConfiguration()
-            .useStoryLoader(new LoadFromClasspath(this.getClass()))
-            .useStoryReporterBuilder(new StoryReporterBuilder()
-                    .withDefaultFormats()
-                    .withFormats(Format.HTML,Format.CONSOLE)
+        .useStoryLoader(new LoadFromClasspath(this.getClass()))
+        .useStoryReporterBuilder(new StoryReporterBuilder()
+            .withDefaultFormats()
+            .withFormats(Format.HTML,Format.CONSOLE)
             .withRelativeDirectory("../../build/jbehave/"));
 
   }
@@ -40,26 +41,26 @@ public class CheckGoogleSearch extends JUnitStories {
     return new InstanceStepsFactory(configuration(), new HomepageSteps(driver));
   }
 
-    @Override
-    protected List<String> storyPaths() {
-        return new StoryFinder().findPaths(CodeLocations.codeLocationFromPath("src/test/resources"),
-                "**/*.story",
-                "**/exclude_*.story");
-    }
+  @Override
+  protected List<String> storyPaths() {
+    return new StoryFinder().findPaths(CodeLocations.codeLocationFromPath("src/test/resources"),
+        "**/*.story",
+        "**/exclude_*.story");
+  }
 
-    private static WebDriver driver(){
-        System.setProperty("webdriver.chrome.driver", "path-to-chromedriver");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("intl.accept_languages", "en-GB");
-        chromeOptions.setExperimentalOption("prefs", prefs);
+  private static WebDriver driver(){
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions chromeOptions = new ChromeOptions();
+    Map<String, Object> prefs = new HashMap<>();
+    prefs.put("intl.accept_languages", "en-GB");
+    chromeOptions.setExperimentalOption("prefs", prefs);
 
 
-        return new ChromeDriver(chromeOptions);
-    }
+    return new ChromeDriver(chromeOptions);
+  }
 
-    @AfterClass
-    public static void close(){
-        driver.close();
-    }
+  @AfterClass
+  public static void close(){
+    driver.close();
+  }
 }
